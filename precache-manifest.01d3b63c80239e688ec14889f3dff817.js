213c995a158d1718f06a989ed6aed00d13cc9440
Yuriy Grinev <a1d516ac5f5d290@gmail.com>
2022-11-17 11:45:04 +0500

    Эх

---
+++ precache-manifest.01d3b63c80239e688ec14889f3dff817.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d41d8cd98f00b204e9800998ecf8427e",
+    "url": "/.gitkeep"
+  },
+  {
+    "revision": "1771371b2174fd5487c9f89b7867c48a",
+    "url": "/browserconfig.xml"
+  },
+  {
+    "revision": "8f2c087a7cb6c88de01d",
+    "url": "/css/app.a65a4d99.css"
+  },
+  {
+    "revision": "f67cca05a7ae32aa68416b433c7c7c15",
+    "url": "/icons/android-chrome-192x192.png"
+  },
+  {
+    "revision": "912a474d713fb2e5dfa1760680d89c45",
+    "url": "/icons/android-chrome-512x512.png"
+  },
+  {
+    "revision": "ae45af2a869762ed9d11e782eae4335e",
+    "url": "/icons/apple-touch-icon.png"
+  },
+  {
+    "revision": "dedd6f1cf1f294032d99db4d56e19197",
+    "url": "/icons/favicon-16x16.png"
+  },
+  {
+    "revision": "a636bc0673e588583b80baf595c69d66",
+    "url": "/icons/favicon-32x32.png"
+  },
+  {
+    "revision": "c5d4b954f3d0716b03b0c1fff1578c90",
+    "url": "/icons/favicon-48x48.png"
+  },
+  {
+    "revision": "2058fee837aa81c65ed9c988e20a4751",
+    "url": "/icons/mstile-150x150.png"
+  },
+  {
+    "revision": "fa2175b2faffd87db5406276daf8a3a6",
+    "url": "/icons/safari-pinned-tab.svg"
+  },
+  {
+    "revision": "02c1d3ead301f579e5aca5739b088f0b",
+    "url": "/img/logo.02c1d3ea.jpg"
+  },
+  {
+    "revision": "0f006f254a7327f6d52279fc9c0752f9",
+    "url": "/img/twitch.0f006f25.png"
+  },
+  {
+    "revision": "84059c2e23d536f058ba3b1141e1d878",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8f2c087a7cb6c88de01d",
+    "url": "/js/app.2b1a7aee.js"
+  },
+  {
+    "revision": "06185b62aac9de75983a",
+    "url": "/js/chunk-vendors.1f43a056.js"
+  },
+  {
+    "revision": "b8b112851c64795fb9e53863b36ca528",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "e2d01717dde0a1adba2189b7742e38ce",
+    "url": "/site.webmanifest"
+  }
+]);
